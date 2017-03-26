6d96a8ad805348d06429e8fae1942eed3f4d5799
SeanJackson1998 <skatts123@gmail.com>
2017-03-26 02:13:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/skatt/seatspace/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.skatt.seatspace;
+
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView availableNo;
+    TextView totalNo;
+    int seats;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = getIntent();
+        final String building = i.getStringExtra("Building");
+        final String floor = i.getStringExtra("Floor");
+        final String room = i.getStringExtra("Room");
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        setContentView(R.layout.activity_main);
+        availableNo = (TextView) findViewById(R.id.availableNo);
+        totalNo = (TextView) findViewById(R.id.totalNo);
+
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                while(seats<20) {
+                    try {
+                        Thread.sleep(500);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    getFile(building, floor, room); // put the building floor and room in to the earch
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+
+    public void getFile(String b, String f, String r) throws Exception {
+        try {
+            // Create a URL for the desired page
+            URL url = new URL("http://kkmonlee.com/launchpad/file.txt");
+            URLConnection connection = url.openConnection();
+
+            // Read all the text returned by the server
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                String[] location = str.split(",");
+                if(location[0].equals(b) && location[1].equals(f) && location[2].equals(r))
+                {
+                    totalNo.setText(location[4]);
+                    availableNo.setText(location[3]);
+                }
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+        } catch (IOException e) {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/skatt/seatspace/RoomOption.java
@@ -0,0 +1,77 @@
+package com.example.skatt.seatspace;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class RoomOption extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+
+        addItemsOnBSpinner();
+        addItemsOnFSpinner();
+        addItemsOnRSpinner();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public void CheckSeats(View view)
+    {
+        Spinner b = (Spinner)findViewById(R.id.spinnerB);
+        String building = b.getSelectedItem().toString();
+        Spinner f = (Spinner)findViewById(R.id.spinnerF);
+        String floor = f.getSelectedItem().toString();
+        Spinner r = (Spinner)findViewById(R.id.spinnerR);
+        String room = r.getSelectedItem().toString();
+
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("Building", building);
+        i.putExtra("Floor", floor);
+        i.putExtra("Room", room);
+        startActivity(i);
+    }
+
+    public void addItemsOnBSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerB);
+        ArrayList<String> Buildings = new ArrayList<>();
+        Buildings.add("Library");
+        Buildings.add("1 West");
+        Buildings.add("10 West");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Buildings);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+    public void addItemsOnFSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerF);
+        ArrayList<String> Floors = new ArrayList<>();
+        Floors.add("Floor 1");
+        Floors.add("Floor 2");
+        Floors.add("Floor 3");
+        Floors.add("Floor 4");
+        Floors.add("Floor 5");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Floors);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+    public void addItemsOnRSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerR);
+        ArrayList<String> Rooms = new ArrayList<>();
+        Rooms.add("2.101");
+        Rooms.add("2.102");
+        Rooms.add("2.103");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Rooms);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+}
