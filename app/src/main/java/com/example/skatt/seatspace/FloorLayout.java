eb7ba0606b0be2cbdb37e08226751072ac23d7cf
Atul <kkmonlee@gmail.com>
2017-03-31 00:43:23 +0100

    Comma separated input from server

--- app/src/main/java/com/example/skatt/seatspace/FloorLayout.java
+++ app/src/main/java/com/example/skatt/seatspace/FloorLayout.java
@@ -38,6 +38,8 @@ public class FloorLayout extends AppCompatActivity {
     Button btn9;
 
     ArrayList<Button> buttons = new ArrayList<>();
+    private int total = 54;
+    private int available = 54;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,10 +67,10 @@ public class FloorLayout extends AppCompatActivity {
         availableNo = (TextView) findViewById(R.id.availableNo);
         totalNo = (TextView) findViewById(R.id.totalNo);
 
-        Thread thread = new Thread(new Runnable(){
+        Thread thread = new Thread(new Runnable() {
             @Override
-            public void run(){
-                while(true) {
+            public void run() {
+                while (true) {
                     try {
                         Thread.sleep(500);
                         runOnUiThread(new Runnable() {
@@ -76,7 +78,7 @@ public class FloorLayout extends AppCompatActivity {
                             public void run() {
                                 try {
                                     getFile(building, floor, room); // put the building floor and room in to the earch
-                                    updateBtns();
+                                    updateButtons();
                                 } catch (Exception e) {
                                     e.printStackTrace();
                                 }
@@ -122,26 +124,20 @@ public class FloorLayout extends AppCompatActivity {
                 String[] location = str.split(",");
                 int currentSeats = Integer.parseInt(location[3]);
                 int totalSeats = Integer.parseInt(location[4]);
-                if(location[0].equals(b) && location[1].equals(f) && location[2].equals(r))
-                {
-                    if(totalSeats < currentSeats)
-                    {
+                if (location[0].equals(b) && location[1].equals(f) && location[2].equals(r)) {
+                    if (totalSeats < currentSeats) {
                         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
                         builder.setMessage("You cannot possibly leave the room.").setTitle("Room is empty");
                         totalNo.setText(location[4]);
                         availableNo.setText(location[4]);
-                    }
-                    else if(currentSeats < 0)
-                    {
+                    } else if (currentSeats < 0) {
                         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
                         builder.setMessage("You cannot enter this room.").setTitle("Room is full");
                         totalNo.setText(location[4]);
                         availableNo.setText("0");
-                    }
-                    else
-                    {
+                    } else {
                         totalNo.setText(location[4]);
                         availableNo.setText(location[3]);
                     }
@@ -156,71 +152,43 @@ public class FloorLayout extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-    private int total = 54;
-    private int available = 54;
-    public void updateBtns() throws Exception {
-        for(int i=1; i<3; i++)
-        {
-            try {
-                // Create a URL for the desired page
-                URL url = new URL("http://kkmonlee.com/launchpad/arduino1.txt");
-                if(i==1)
-                {
-                    URLConnection connection = url.openConnection();
 
-                    // Read all the text returned by the server
-                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String str;
+    public void updateButtons() {
+        try {
+            // Create a URL for the desired page
+            URL url = new URL("http://kkmonlee.com/launchpad/arduino1.txt");
+            URLConnection connection = url.openConnection();
 
-                    while ((str = in.readLine()) != null) {
-                        if(str.equals("1"))
-                        {
-                            btn1.setBackgroundColor(Color.RED);
-                            available--;
-                            availableNo.setText(String.valueOf(available));
-                        }
-                        else
-                        {
-                            btn1.setBackgroundColor(Color.GREEN);
-                            available++;
-                            availableNo.setText(String.valueOf(available));
-                        }
+            // Read all the text returned by the server
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                String[] inputArray = str.split(",");
+
+                for (int i = 0; i < inputArray.length; i++) {
+                    if (inputArray[i].equals("1")) {
+                        buttons.get(i).setBackgroundColor(Color.RED);
+                        available--;
+                        availableNo.setText(String.valueOf(available));
+                    } else if (inputArray[i].equals("0")) {
+                        buttons.get(i).setBackgroundColor(Color.GREEN);
+                        available++;
+                        availableNo.setText(String.valueOf(available));
                     }
-                    in.close();
+
                 }
-                if(i==2)
-                {
-                    URLConnection connection = url.openConnection();
-
-                    // Read all the text returned by the server
-                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String str;
-
-                    while ((str = in.readLine()) != null) {
-                        if(str.equals("1"))
-                        {
-                            btn2.setBackgroundColor(Color.RED);
-                        }
-                        else
-                        {
-                            btn2.setBackgroundColor(Color.GREEN);
-                        }
-                    }
-                    in.close();
-                }
-
-            } catch (MalformedURLException e) {
-                System.out.println("YOU FUCKED UP.");
-            } catch (IOException e) {
-                System.out.println("YOU FUCKED UP.");
             }
+            in.close();
+        } catch (IOException e) {
+            System.out.println("YOU FUCKED UP.");
         }
     }
+
     /*
     Home button
      */
-    public void home(View view)
-    {
+    public void home(View view) {
         Intent i = new Intent(this, HomeScreen.class);
         startActivity(i);
     }
